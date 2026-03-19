8562c57605db1b4eab3cd74bd49ba4257e0eae03
igordg777 <igordg@mail.ru>
2026-03-19 14:59:30 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const app = express();
+const path = require('path');
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// Подключаем статику
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Подключаем views(hbs)
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'hbs');
+
+// Отображаем главную страницу с использованием шаблона "index.hbs"
+app.get('/', function (req, res) {
+    res.render('index', req.query);
+});
+
+// Отображаем контакты с использованием шаблона "contacts.hbs"
+app.get('/contacts', function (req, res) {
+    res.render('contacts', req.query);
+});
+
+
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log("Listening on " + port));
