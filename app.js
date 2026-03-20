94f982d873a340f385436959d6b3cfec72da73be
КК-Варшавская-206 Студент <edu-msk29-206s@synergy.local>
2026-03-20 10:19:47 +0300

    promo

--- app.js
+++ app.js
@@ -21,11 +21,20 @@ app.get('/contacts', function (req, res) {
     res.render('contacts', req.query);
 });
 
+// Отображаем контакты с использованием шаблона "about_us.hbs"
+app.get('/about_us', function (req, res) {
+    res.render('about_us', req.query);
+});
+
+// Отображаем контакты с использованием шаблона "profile.hbs"
+app.get('/profile', function (req, res) {
+    res.render('profile', req.query);
+});
+
 // Отображаем Акции с использованием шаблона "promo.hbs"
 app.get('/promo', function (req, res) {
     res.render('promo', req.query);
 });
-
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log("Listening on " + port));
